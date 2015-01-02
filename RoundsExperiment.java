3b43a025e1c4331e6890a7e789dc750401f1981c
adiagarwalla <aa4@princeton.edu>
2015-01-02 11:53:12 +0530

    Experiment added

---
+++ RoundsExperiment.java
@@ -0,0 +1,86 @@
+/** 
+ * Helen Yu (heleny)
+ * Aditya Agarwalla (aa4)
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class RoundsExperiment {
+
+    // Main method for systematic experiment for AdaBoost rounds testing
+    // Requires four arguments to run
+    // E.g.java RoundsExperiment data/dna_500 data/census_1000 data/ocr17_1000 
+    // data/ocr49_1000
+    public static void main(String argv[]) throws FileNotFoundException, 
+                                                  IOException
+    {
+        if (argv.length < 4) 
+        {
+            System.out.println("RoundsExperiment file1 file2 file3 file4"); 
+            return;
+        }
+
+        String[] filestem = new String[4];
+
+        for (int i = 0; i < 4; i++)
+            filestem[i] = argv[i];
+
+        DataSet[] d = new DataSet[4];
+        for (int i = 0; i < 4; i++)
+            d[i] = new DiscreteDataSet(filestem[i]);
+
+        for (int i = 0; i < 4; i++)
+        {
+            System.out.println("DataSet is: " + filestem[i]);
+
+            for (int rounds = 100; rounds <= 2000; rounds += 100)
+            {
+                // Sum of errors across all trials
+                double sumError = 0.0;
+
+                // Number of trials
+                int trial;
+
+                for (trial = 0; trial < 10; trial++)
+                {
+                    Classifier c = new AdaBoost(d[i], rounds);
+
+                    d[i].printTestPredictions(c, filestem[i]);
+        	
+                    FileReader frTO = new FileReader(filestem[i]+".testout");
+                    FileReader frAns = new FileReader(filestem[i]+".answers");
+
+                    Scanner scannerTO = new Scanner(frTO);
+                    Scanner scannerAns = new Scanner(frAns);
+
+                    int skip = 0;
+
+                    while (skip < 4) {
+                        String line = scannerTO.nextLine();
+                        skip++;
+                    }
+
+                    int error = 0;
+                    int numLines = 0;
+
+                    while (scannerTO.hasNextLine())
+                    {
+                        String TO_Ans = scannerTO.nextLine();
+                        String Ans = scannerAns.nextLine();
+
+                        numLines++;
+
+                        if (!TO_Ans.equals(Ans))
+                            error++;
+                    }
+
+                    sumError += (double)error/numLines;
+                }
+
+                System.out.println("Error rate average over " + trial + 
+                   " trials " + "for " + rounds + " rounds: " + sumError/trial);
+            }
+        }
+    }
+}
--- data/GenerateTest.java
+++ data/GenerateTest.java
@@ -1,3 +1,8 @@
+/** 
+ * Helen Yu (heleny)
+ * Aditya Agarwalla (aa4)
+ */
+
 import java.util.Scanner;
 import java.io.IOException;
 import java.io.FileReader;
@@ -5,6 +10,11 @@ import java.io.PrintWriter;
 
 public class GenerateTest {
 
+	// Splits training dataset into two parts - one for training and one for
+	// testing. Generates .train file for RoundsExperiment to take in as
+	// training set, .test file to take in as testing set, .answers to compare
+	// RoundsExperiment's testout to for accuracy and .names for use by
+	// RoundsExperiment
     public static void main (String[] args) throws IOException{ 
 	if (args.length < 2) {
 	    System.out.println("GenerateTest filestem trainingSetSize"); 
@@ -19,6 +29,17 @@ public class GenerateTest {
 	PrintWriter test = new PrintWriter(filestem2+".test");
 	PrintWriter answers = new PrintWriter(filestem2+".answers");
 	PrintWriter train = new PrintWriter(filestem2+".train");
+
+	FileReader fr1 = new FileReader(filestem+".names");
+	PrintWriter names = new PrintWriter(filestem2+".names");
+	Scanner scannerNames = new Scanner(fr1);
+
+	while (scannerNames.hasNextLine()) {
+		String line = scannerNames.nextLine();
+		names.println(line);
+	}
+	names.close();
+
 	int count = 0;
 	while (scanner.hasNextLine()) {
 	    String line = scanner.nextLine();
@@ -39,9 +60,4 @@ public class GenerateTest {
 	answers.close();
 	
     }
-
-
-
-
-
 }
